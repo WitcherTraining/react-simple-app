df03e43f08dbe63a7f164e36f326a3d1ec04bf59
WitcherTraining <witcher236@gmail.com>
2020-11-30 23:45:53 +0600

    Init commit for react app counter and todo apps

---
+++ src/components/examples/SecondComponent.jsx
@@ -0,0 +1,15 @@
+const { Component } = require("react")
+
+import React, {Component} from 'react';
+
+class SecondComponent extends Component {
+    render() {
+      return (
+        <div className="secondComponent">
+          Second component
+        </div>
+      );
+    }
+  }
+
+  export default SecondComponent;
